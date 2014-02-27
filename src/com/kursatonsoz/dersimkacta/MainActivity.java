1e77dd65bedf35489a9957d9abec6391f233afda
Kürşat Önsöz <kursatt@kursatt.(none)>
2014-02-27 21:35:04 +0200

    First Commit

---
+++ src/com/kursatonsoz/dersimkacta/MainActivity.java
@@ -0,0 +1,39 @@
+package com.kursatonsoz.dersimkacta;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button kacta = (Button) findViewById(R.id.button1);
+        
+        kacta.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(new Intent("com.kursatonsoz.dersimkacta.KACTA"));
+				
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
